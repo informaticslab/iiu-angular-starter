1e8c1b459989e90710eb52c778e51b7fa5685b0b
Michael Ta <technical.ta@gmail.com>
2017-02-01 09:48:03 -0500

    Add base project for starter
    Includes all of the neccesary foundation for the app to be ran with Webpack

---
+++ src/client/app/app.js
@@ -0,0 +1,29 @@
+import angular from 'angular';
+import 'bootstrap/dist/css/bootstrap.css';
+import routing from './app.config';
+import uirouter from 'angular-ui-router';
+
+import '../style/app.css';
+
+let app = () => {
+  return {
+    template: require('./app.html'),
+    controller: 'AppCtrl',
+    controllerAs: 'app'
+  }
+};
+
+class AppCtrl {
+  constructor() {
+    this.url = 'https://github.com/preboot/angular-webpack';
+  }
+}
+
+const MODULE_NAME = 'app';
+
+angular.module(MODULE_NAME, [uirouter])
+  .config(routing)
+  .directive('app', app)
+  .controller('AppCtrl', AppCtrl);
+
+export default MODULE_NAME;
