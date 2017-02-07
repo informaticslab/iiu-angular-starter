2b488e319b0fa1e1ef8d6779514d010a3d99f3ab
Michael Ta <technical.ta@gmail.com>
2017-02-07 12:19:25 -0500

    Update configuration files
    Updated package.json dependencies. Updated webpack configuration to transpile ES6 to ES5 properly.  Added missing babelrc file.

--- webpack.config.js
+++ webpack.config.js
@@ -190,13 +190,6 @@ module.exports = function makeWebpackConfig() {
   // Add build specific plugins
   if (isProd) {
     config.plugins.push(
-      // Reference: http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
-      // Only emit files when there are no errors
-      new webpack.NoErrorsPlugin(),
-
-      // Reference: http://webpack.github.io/docs/list-of-plugins.html#dedupeplugin
-      // Dedupe modules in the output
-      new webpack.optimize.DedupePlugin(),
 
       // Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
       // Minify all javascript, switch loaders to minimizing mode
@@ -221,4 +214,4 @@ module.exports = function makeWebpackConfig() {
   };
 
   return config;
-} ();
+}();
